4eca21d078e16adfe4012758a1d72b5613000a02
Samuel Thomas <samthoma@iu.edu>
2019-09-21 18:43:02 -0400

    begin coding

--- hmclearn/R/mcmc_functions.R
+++ hmclearn/R/mcmc_functions.R
@@ -76,27 +76,52 @@ qfun_all <- function(theta1, theta2, nu) {
 # logDENS:  log of joint density of parameter of interest
 #   (log likelihood)
 # ... additional parameters to pass to logDENS
-leapfrog <- function(theta, r, epsilon, logDENS, y, X, ...) {
-  require(pracma)
-  g.ld <- grad(logDENS, theta, y=y, X=X, ...)
+leapfrog <- function(theta_lf, r, epsilon, logPOSTERIOR, glogPOSTERIOR, y, X, Z, Minv, constrain,
+                     lastSTEP=FALSE, ...) {
+
+  # gradient of log posterior for old theta
+  g.ld <- glogPOSTERIOR(theta_lf, y=y, X=X, Z=Z, ...)
+
+  # first momentum update
   r.new <- r + epsilon/2*g.ld
-  theta.new <- theta + epsilon*r.new
-  g.ld.new <- grad(logDENS, theta.new, y=y, X=X, ...)
-  r.new <- r.new + epsilon/2*g.ld.new
+
+  # theta update
+  # note diagonal matrix update
+  # theta.new <- theta_lf + as.numeric(epsilon*r.new/ diag(M_mx))
+  theta.new <- theta_lf + as.numeric(epsilon* Minv %*% r.new)
+
+  # check positive
+  switch_sign <- constrain & theta.new < 0
+  r.new[switch_sign] <- -r.new[switch_sign]
+  theta.new[switch_sign] <- -theta.new[switch_sign]
+
+  # gradient of log posterior for new theta
+  g.ld.new <- glogPOSTERIOR(theta.new, y=y, X=X, Z=Z, ...)
+
+  # if not on last step, second momentum update
+  if (!lastSTEP) {
+    r.new <- r.new + epsilon/2*g.ld.new
+  }
   list(theta.new=theta.new,
-       r.new=r.new)
+       r.new=as.numeric(r.new))
 }
+
+
 
 # theta.init:  initial values of theta
 # Nstep:  number of steps (called L in paper)
-# M:  number of times to repeat
+# N:  number of times to repeat
 # epsilon:  step size
-# logDENS:  log of joint density of parameter of interest
-# ...:  additional parameters to pass to logDENS
-hmc <- function(M, theta.init, epsilon, Nstep, logDENS, y, X, verbose=FALSE, ...) {
-  require(MASS)
-  p <- length(theta.init) # number of parameters
+# logPOSTERIOR:  log of joint density of parameter of interest
+# ...:  additional parameters to pass to logPOSTERIOR
+hmc <- function(N, theta.init, epsilon, L, logPOSTERIOR, glogPOSTERIOR, y, X, Mdiag=NULL,
+                verbose=FALSE, ...) {
+  p <- length(theta.init)
   mu.p <- rep(0, p)
+
+  if (is.null(Mdiag)) {
+    Mdiag <- rep(1, length(theta.init))
+  }
 
   # store theta and momentum (usually not of interest)
   theta <- list()
@@ -104,12 +129,13 @@ hmc <- function(M, theta.init, epsilon, Nstep, logDENS, y, X, verbose=FALSE, ...
   r <- list()
   r[[1]] <- NA
   accept <- 0
-  for (m in 2:M) {
+  for (m in 2:N) {
     theta[[m]] <- theta.new <- theta[[m-1]]
-    r0 <- mvrnorm(1, mu.p, diag(p))
+    # r0 <- MASS::mvrnorm(1, mu.p, diag(p))
+    r0 <- mapply(rnorm, mean=mu.p, sd=Mdiag, MoreArgs=list(n=1))
     r.new <- r[[m]] <- r0
-    for (i in 1:Nstep) {
-      lf <- leapfrog(theta.new, r.new, epsilon, logDENS, y, X, ...)
+    for (i in 1:L) {
+      lf <- leapfrog(theta.new, r.new, epsilon, logPOSTERIOR, glogPOSTERIOR, y, X, ...)
       theta.new <- lf$theta.new
       r.new <- lf$r.new
     }
@@ -120,8 +146,8 @@ hmc <- function(M, theta.init, epsilon, Nstep, logDENS, y, X, verbose=FALSE, ...
     u <- runif(1)
 
     # use log transform for ratio due to low numbers
-    log.alpha <- pmin(0, logDENS(theta.new, y=y, X=X, ...) - 0.5*(r.new %*% r.new) -
-                        logDENS(theta[[m-1]], y=y, X=X, ...) - 0.5*(r0 %*% r0))
+    log.alpha <- pmin(0, logPOSTERIOR(theta.new, y=y, X=X, ...) - 0.5*(r.new %*% r.new) -
+                        logPOSTERIOR(theta[[m-1]], y=y, X=X, ...) - 0.5*(r0 %*% r0))
 
     if (log.alpha < log(u)) {
       theta[[m]] <- theta.new
@@ -131,8 +157,6 @@ hmc <- function(M, theta.init, epsilon, Nstep, logDENS, y, X, verbose=FALSE, ...
       theta[[m]] <- theta[[m-1]]
       r[[m]] <- r[[m-1]]
     }
-
-    # print(m)
 
   }
   list(theta=theta,
