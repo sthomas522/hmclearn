89e796bc0219a5cab7869c664ba9310fc69c8f33
Sam Thomas <samuel.thomas522@gmail.com>
2019-11-04 16:30:58 -0500

    linear regression

---
+++ hmclearn/R/hmc_linear.R
@@ -0,0 +1,25 @@
+linear_posterior <- function(theta, y, X, a=1e-4, b=1e-4) {
+  k <- length(theta)
+  beta_param <- theta[1:(k-1)]
+  gamma_param <- theta[k]
+
+  n <- nrow(X)
+  -(n/2 + a)*gamma_param - exp(-gamma_param)/2 * t(y - X%*%beta_param) %*%
+    (y - X%*%beta_param) - b*exp(-gamma_param)
+}
+
+g_linear_posterior <- function(theta, y, X, a=1e-4, b=1e-4) {
+  # parameters
+  k <- length(theta)
+  beta_param <- theta[1:(k-1)]
+  gamma_param <- theta[k]
+  n <- nrow(X)
+
+  # gradient of beta
+  grad_beta <- exp(-gamma_param)  * t(X) %*% (y - X%*%beta_param)
+  grad_gamma <- -(n/2 + a) + exp(-gamma_param)/2 * t(y - X%*%beta_param) %*%
+    (y - X%*%beta_param) + b*exp(-gamma_param)
+  c(as.numeric(grad_beta), as.numeric(grad_gamma))
+}
+
+
