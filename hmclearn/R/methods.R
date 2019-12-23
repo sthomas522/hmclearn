38b05be125c5a6d4facaf50c3301a0c003de91f1
Samuel Thomas <samthoma@iu.edu>
2019-12-23 13:32:59 -0500

    predict lm

--- hmclearn/R/methods.R
+++ hmclearn/R/methods.R
@@ -20,6 +20,25 @@ coef.hmclearn <- function(x, burnin=100, prob=0.5, ...) {
 
 
 #' @export
-predict.hmclearn <- function(x, ...) {
-  1
+predict.hmclearn <- function(object, X, fam = "linear", burnin=100, nsamp=NULL, ...) {
+
+  thetaDF <- object$thetaDF[-c(1:burnin), ]
+  k <- ncol(thetaDF)
+
+  if (is.null(nsamp)) {
+    nsamp <- nrow(thetaDF)
+  }
+  index <- 1:nsamp
+  sampvals <- sample(index, size=nsamp, replace = FALSE)
+
+  preds <- NULL
+  if (fam == "linear") {
+    preds <- sapply(sampvals, function(xx, dat=thetaDF, bparam=1:(k-1), sigparam=k) {
+      betaval <- as.numeric(dat[xx, bparam])
+      sigval <- sqrt(exp(dat[xx, sigparam]))
+      z <- rnorm(1, mean=X%*%betaval, sd=sigval)
+      z
+    })
+  }
+  return(preds)
 }
