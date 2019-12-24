76192feaf8ffff9514302cf6f4b33916a1d78fc7
Samuel Thomas <samthoma@iu.edu>
2019-12-24 11:35:55 -0500

    more bayesplot

--- hmclearn/R/plotfun.R
+++ hmclearn/R/plotfun.R
@@ -5,8 +5,11 @@ mcmc_intervals <- function(object, ...) {
 }
 
 #' @export
-mcmc_intervals.hmclearn <- function(object, ...) {
+mcmc_intervals.hmclearn <- function(object, burnin=NULL, ...) {
   data <- object$thetaDF
+  if (!is.null(burnin)) {
+    data <- data[-c(1:burnin), ]
+  }
   bayesplot::mcmc_intervals(data, ...)
 }
 
@@ -16,10 +19,91 @@ mcmc_areas <- function(object, ...) {
 }
 
 #' @export
-mcmc_areas.hmclearn <- function(object, ...) {
+mcmc_areas.hmclearn <- function(object, burnin=NULL, ...) {
   data <- object$thetaDF
+  if (!is.null(burnin)) {
+    data <- data[-c(1:burnin), ]
+  }
   bayesplot::mcmc_areas(data, ...)
 }
+
+
+#' @export
+mcmc_hist <- function(object, ...) {
+  UseMethod("mcmc_hist")
+}
+
+#' @export
+mcmc_hist.hmclearn <- function(object, burnin=NULL, ...) {
+  data <- object$thetaDF
+  if (!is.null(burnin)) {
+    data <- data[-c(1:burnin), ]
+  }
+  bayesplot::mcmc_hist(data, ...)
+}
+
+
+
+#' @export
+mcmc_dens <- function(object, ...) {
+  UseMethod("mcmc_dens")
+}
+
+#' @export
+mcmc_dens.hmclearn <- function(object, burnin=NULL, ...) {
+  data <- object$thetaDF
+  if (!is.null(burnin)) {
+    data <- data[-c(1:burnin), ]
+  }
+  bayesplot::mcmc_dens(data, ...)
+}
+
+
+#' @export
+mcmc_scatter <- function(object, ...) {
+  UseMethod("mcmc_scatter")
+}
+
+#' @export
+mcmc_scatter.hmclearn <- function(object, burnin=NULL, ...) {
+  data <- object$thetaDF
+  if (!is.null(burnin)) {
+    data <- data[-c(1:burnin), ]
+  }
+  bayesplot::mcmc_scatter(data, ...)
+}
+
+
+#' @export
+mcmc_hex <- function(object, ...) {
+  UseMethod("mcmc_hex")
+}
+
+#' @export
+mcmc_hex.hmclearn <- function(object, burnin=NULL, ...) {
+  data <- object$thetaDF
+  if (!is.null(burnin)) {
+    data <- data[-c(1:burnin), ]
+  }
+  bayesplot::mcmc_hex(data, ...)
+}
+
+
+# requires multiple chains
+#' #' @export
+#' mcmc_violin <- function(object, ...) {
+#'   UseMethod("mcmc_violin")
+#' }
+#'
+#' #' @export
+#' mcmc_violin.hmclearn <- function(object, burnin=NULL, ...) {
+#'   data <- object$thetaDF
+#'   if (!is.null(burnin)) {
+#'     data <- data[-c(1:burnin), ]
+#'   }
+#'   bayesplot::mcmc_violin(data, ...)
+#' }
+
 
 #' #' @export
 #' color_scheme_set <- function(...) {
