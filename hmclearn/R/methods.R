9af42f45dec62790e15ccfed086fb93968d5bea6
Samuel Thomas <samthoma@iu.edu>
2019-12-28 11:04:52 -0500

    docs

--- hmclearn/R/methods.R
+++ hmclearn/R/methods.R
@@ -1,19 +1,4 @@
 
-
-# combArray <- function(x, burnin, marg=3) {
-#   xnew <- x[-c(1:burnin), 1:dim(x)[2], 1:dim(x)[3]]
-#   if (length(dim(xnew)) > 2) {
-#     xnew <- aperm(xnew, c(3, 1, 2))
-#     xnew <- matrix(xnew, prod(dim(xnew)[1:2]), dim(xnew)[3])
-#   } else {
-#     xnew <- apply(xnew, 2, as.numeric)
-#   }
-#   print(class(xnew))
-#   print(apply(xnew, 2, class))
-#   xnewDF <- as.data.frame(xnew)
-#   return(xnewDF)
-# }
-
 combMatrix <- function(x, burnin) {
   if (is.null(burnin)) {
     return(x)
@@ -25,6 +10,15 @@ combMatrix <- function(x, burnin) {
   xnew
 }
 
+
+#' Summarizing HMC Model Fits
+#'
+#' summary method for class \code{hmclearn}
+#'
+#' @param x an object of class \code{hmclearn}, usually a result of a call to \code{mh} or \code{hmc}
+#' @param burnin optional numeric parameter for the number of initial MCMC samples to omit from the summary
+#' @param probs quantiles to summarize the posterior distribution
+#' @param ... additional arguments to pass to \code{quantile}
 #' @export
 summary.hmclearn <- function(x, burnin=NULL, probs=c(0.05, 0.25, 0.5, 0.75, 0.95), ...) {
   cat("Summary of HMC simulation\n\n")
