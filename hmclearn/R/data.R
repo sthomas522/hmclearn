6f6d617d249ba8592a51a9b031a1e5681d4bd785
Samuel Thomas <samthoma@iu.edu>
2019-12-31 17:01:58 -0500

    Endometrial

--- hmclearn/R/data.R
+++ hmclearn/R/data.R
@@ -16,3 +16,21 @@
 #' @references Fox, G. A., Negrete-Yankelevich, S., & Sosa, V. J. (Eds.). (2015). \emph{Ecological statistics: contemporary theory and application}. Oxford University Press, USA.
 #' @source \url{https://bbolker.github.io/mixedmodels-misc/ecostats_chap.html}
 "Gdat"
+
+
+
+#' Endometrial Cancer Dataset
+#'
+#' Data from a study about Endometrial Cancer
+#'
+#' @format A data frame with 79 rows and 4 variables:
+#' \describe{
+#'   \item{NV}{Neovasculation risk factor indicator (0=Absent, 1=Present)}
+#'   \item{PI}{Pulsatility index of arteria uterina}
+#'   \item{EH}{Endometrium height}
+#'   \item{HG}{histology of patient (0=Low, 1=High)}
+#' }
+#' @references Agresti, A. (2015). \emph{Foundations of linear and generalized linear models}. John Wiley & Sons.
+#' @references Heinze, G., & Schemper, M. (2002). \emph{A solution to the problem of separation in logistic regression}. Statistics in medicine, 21(16), 2409-2419.
+#' @source \url{http://users.stat.ufl.edu/~aa/glm/data/Endometrial.dat}
+"Endometrial"
