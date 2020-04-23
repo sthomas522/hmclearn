4b718743dce6f06164cc43f235226084c3b5fd55
Samuel Thomas <samthoma@iu.edu>
2020-04-23 11:23:28 -0400

    tests

--- tests/testthat/test_paper.R
+++ tests/testthat/test_paper.R
@@ -2,7 +2,7 @@ test_that("paper examples", {
   # Linear regression example
   y <- warpbreaks$breaks
   X <- model.matrix(breaks ~ wool*tension, data=warpbreaks)
-  N <- 1e4
+  N <- 2e3
   set.seed(143)
 
   eps_vals <- c(rep(2e-1, 6), 2e-2)
@@ -13,16 +13,24 @@ test_that("paper examples", {
                  logPOSTERIOR = linear_posterior,
                  glogPOSTERIOR = g_linear_posterior,
                  varnames = c(colnames(X), "log_sigma_sq"),
-                 param=list(y=y, X=X))
+                 param=list(y=y, X=X), chains=2,
+                 parallel=FALSE)
   t2 <- Sys.time()
 
   # test values
   c1_hmc <- as.vector(round(coef(fm1_hmc), 6))
 
-  test1 <- c(43.013150, -14.012657, -18.327812, -18.018012,
-             18.005013, 7.972607, 4.798094)
+  test1 <- c(42.950208, -13.744052, -18.029510, -17.597738,
+             17.869968, 7.675399, 4.800039)
 
   expect_equal(c1_hmc, test1)
+
+  p1_hmc <- as.vector(round(psrf(fm1_hmc), 6))
+
+  test1b <- c(0.999787, 1.000431, 1.008846, 1.005493, 1.008580, 0.999912,
+              0.999788)
+
+  expect_equal(p1_hmc, test1b)
 
   #######################################
   # logistic regression
@@ -43,7 +51,7 @@ test_that("paper examples", {
                     data = birthwt2)
   y <- birthwt2$low
 
-  N <- 1e4
+  N <- 2e3
 
   # use different epsilon values for continuous and dichotomous variables
   continuous_ind <- c(FALSE, TRUE, TRUE, rep(FALSE, 8))
@@ -56,17 +64,26 @@ test_that("paper examples", {
                  logPOSTERIOR = logistic_posterior,
                  glogPOSTERIOR = g_logistic_posterior,
                  param=list(y=y, X=X),
-                 varnames = colnames(X))
+                 varnames = colnames(X),
+                 chains=2, parallel=FALSE)
   t2 <- Sys.time()
 
   # test values
   c2_hmc <- as.vector(round(coef(fm2_hmc), 6))
 
-  test2 <- c(0.759534, -0.034473, -0.016576, 1.238999, 0.785729,
-             0.777453, 1.421063, 2.027768, 0.709706, -0.480946,
-             0.151321)
+  test2 <- c(0.972062, -0.040255, -0.016622, 1.194040, 0.730470,
+             0.748092, 1.435274, 2.021169, 0.674046, -0.483113,
+             0.140353)
 
   expect_equal(c2_hmc, test2)
+
+  p2_hmc <- as.vector(round(psrf(fm2_hmc), 6))
+
+  test2b <- c(1.004376, 1.003049, 1.005980, 1.002901, 1.002973,
+              1.000847, 1.000158, 1.010873, 0.999796, 1.004207,
+              1.001912)
+
+  expect_equal(p2_hmc, test2b)
 
   ############################################
   # Poisson mixed effects model
@@ -91,41 +108,39 @@ test_that("paper examples", {
   y <- Gdat$shells
   p <- ncol(X)
 
-  # shorten simulation for testing
-  # N <- 1e5
-  N <- 1e3
+  N <- 2e3
 
   set.seed(412)
   initvals <- c(rep(0, 4),
-                rnorm(10, mean=0, sd=1e-3),
+                rep(0, 10),
                 0)
 
-  M_vals <- c(1e-3, 1e-3, 1e-3, 1,
-              rep(1e-3, 10),
-              1e-3)
-
-
-  eps_vals <- c(rep(1e-3, 3), 1e-4, rep(2e-4, 11))
+  eps_vals <- c(3e-2, 3e-2, 3e-2, 1e-3, rep(1e-1, 10), 5e-2)
 
   t1.hmc <- Sys.time()
-  res <- hmc(N = N, theta.init = initvals, epsilon = eps_vals, L = 10,
-             logPOSTERIOR  = glmm_poisson_posterior,
-             Mdiag = M_vals,
-             varnames=c(colnames(X), paste0("u", 1:ncol(Z)), "lambda"),
-             glogPOSTERIOR  = g_glmm_poisson_posterior,
-             param=list(y = y, X=X, Z=Z, m=10, nulambda=1, Alambda=25))
+  fm3_hmc <- hmc(N = N, theta.init = initvals, epsilon = eps_vals, L = 10,
+                 logPOSTERIOR  = glmm_poisson_posterior,
+                 glogPOSTERIOR  = g_glmm_poisson_posterior,
+                 varnames=c(colnames(X), paste0("u", 1:ncol(Z)), "lambda"),
+                 param=list(y = y, X=X, Z=Z, m=10, nulambda=1, Alambda=25),
+                 chains=2, parallel=FALSE)
   t2.hmc <- Sys.time()
-  # t2.hmc - t1.hmc
-  # res$accept/N
+
 
   # test values
-  c3_hmc <- as.vector(round(coef(res), 6))
+  c3_hmc <- as.vector(round(coef(fm3_hmc), 6))
 
-  test3 <- c(0.195788, -0.660140, -0.319137, 0.015836, -1.135854,
-    -0.195184, -0.551731, 0.574802, -0.163860, 0.548948,
-    0.173786, -0.202746, 0.444318, -0.699641, 0.267427)
-
+  test3 <- c(-0.128152, -0.659300, -0.376244, 0.023075, -0.810004,
+             -0.177803, -0.559731, 0.731077, -0.096974, 1.137754,
+             0.237018, -0.186022, 0.955278, -1.020425, -0.359269)
   expect_equal(c3_hmc, test3)
 
+  p3_hmc <- as.vector(round(psrf(fm3_hmc), 6))
+
+  test3b <- c(1.001932, 1.004222, 1.005456, 1.000652, 1.000063,
+              1.000191, 0.999752, 1.001953, 0.999967, 1.000052,
+              1.000999, 1.003302, 0.999750, 0.999764, 1.000016)
+
+  expect_equal(p3_hmc, test3b)
 
 })
